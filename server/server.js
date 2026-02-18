12ebf45487b9b72752e3ea27521fa3df5c8174fa
sachin <sachinn.t279@gmail.com>
2026-02-18 21:41:15 +0530

    fix vercel error

--- server/server.js
+++ server/server.js
@@ -5,19 +5,19 @@ const mongoose = require("mongoose");
 
 const app = express();
 
-const cors = require("cors");
-
 app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://whisperwall-s.vercel.app"
+      "https://whisperwall-s.vercel.app",
     ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
 );
+
 app.use(express.json());
- 
+
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/confessions", require("./routes/confessionRoutes"));
 app.use("/api/admin", require("./routes/adminRoutes"));
@@ -28,8 +28,9 @@ mongoose
   .then(() => {
     console.log("MongoDB Connected");
 
-    app.listen(5000, () => {
-      console.log("Server running on port 5000");
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
     });
   })
   .catch((err) => console.log(err));
