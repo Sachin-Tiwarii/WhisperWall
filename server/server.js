1ee7905f3530e078ce15eaea538cd25f63d1da95
sachin <sachinn.t279@gmail.com>
2026-02-18 22:58:41 +0530

    fix cors properly

--- server/server.js
+++ server/server.js
@@ -1,36 +1,15 @@
-require("dotenv").config();
-const express = require("express");
 const cors = require("cors");
-const mongoose = require("mongoose");
-
-const app = express();
 
 app.use(
   cors({
     origin: [
       "http://localhost:5173",
       "https://whisperwall-s.vercel.app",
+      "https://whisperwall-5tbvksexp-sachins-projects-4df84b7d.vercel.app"
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
 );
 
-app.use(express.json());
-
-app.use("/api/auth", require("./routes/authRoutes"));
-app.use("/api/confessions", require("./routes/confessionRoutes"));
-app.use("/api/admin", require("./routes/adminRoutes"));
-app.use("/api/reports", require("./routes/reportRoutes"));
-
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("MongoDB Connected");
-
-    const PORT = process.env.PORT || 5000;
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
-  })
-  .catch((err) => console.log(err));
+app.options("*", cors());
